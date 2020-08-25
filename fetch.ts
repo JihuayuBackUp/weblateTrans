a1e247f7ff72f2ca14d2d3fc77d6be9bdbc46258
jihuayu <8042833@qq.com>
2020-08-25 15:36:59 +0800

    CI

--- fetch.ts
+++ fetch.ts
@@ -7,7 +7,7 @@ const twitchBase = "https://addons-ecs.forgesvc.net/";
 
 export async function fetch_new_project(from: number, version: string, modloader?: string, to = Date.now()) {
     let ret = [];
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 10; i++) {
         let num = 0;
         const g = await got.get(twitchBase + `api/v2/addon/search?gameId=432&index=${10 * i}&gameVersion=${version}&pageSize=10&sort=1&sectionId=6`)
         const json = JSON.parse(g.body);
@@ -22,17 +22,20 @@ export async function fetch_new_project(from: number, version: string, modloader
                 if (versions.includes(version) && !modloader || versions.includes(modloader)) {
                     const dl = await got.get(file.downloadUrl);
                     const zip = new AdmZip(dl.rawBody);
-                    const lang = zip.getEntries().map(i => {
-                        if (/lang\/.+\.json/g.test(i.entryName)) {
+                    zip.getEntries().map(i => {
+                        if (/assets\/.+\/lang\/.+\.json/g.test(i.entryName)) {
                             const modid = i.entryName.replace(/assets\/(.+)\/lang\/.+/, "$1")
                             const lang_name = i.entryName.replace(/assets\/.+\/lang\/(.+)/, "$1")
-                            const file_path = path.join(__dirname, "maven","minecraft_1-16_modtranslationresourcepack", modid, project.slug, lang_name)
+                            const file_path = path.join(__dirname, "maven","minecraft_1-16_modtranslationresourcepack", project.slug,modid, lang_name)
                             fs.mkdirSync(path.dirname(file_path), {recursive: true});
                             if (i.entryName.endsWith("zh_cn.json") && fs.existsSync(file_path))
                                 return;
                             fs.writeFileSync(file_path, i.getData());
                         }
                     })
+                    const file_path = path.join(__dirname, "maven","minecraft_1-16_modtranslationresourcepack", project.slug, 'project.json');
+                    fs.mkdirSync(path.dirname(file_path),{recursive: true});
+                    fs.writeFileSync(file_path, JSON.stringify({name:project.name,id:project.id}));
                     break;
                 }
 
