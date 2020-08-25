a8a95db7da3a8f2b686df3138a70d93bf5895ba8
jihuayu <8042833@qq.com>
2020-08-25 14:39:49 +0800

    add script

---
+++ fetch.ts
@@ -0,0 +1,47 @@
+import got from 'got'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as AdmZip from "adm-zip";
+
+const twitchBase = "https://addons-ecs.forgesvc.net/";
+
+export async function fetch_new_project(from: number, version: string, modloader?: string, to = Date.now()) {
+    let ret = [];
+    for (let i = 0; i < 10; i++) {
+        let num = 0;
+        const g = await got.get(twitchBase + `api/v2/addon/search?gameId=432&index=${10 * i}&gameVersion=${version}&pageSize=10&sort=1&sectionId=6`)
+        const json = JSON.parse(g.body);
+        for (let i of json) {
+            const files_req = await got.get(twitchBase + `api/v2/addon/${i.id}/files`);
+            const files = JSON.parse(files_req.body);
+            const sorted_file = files.sort((a, b) => {
+                return new Date(b.fileDate).getTime() - new Date(a.fileDate).getTime();
+            })
+            for (let file of sorted_file) {
+                const versions = file.gameVersion as string[];
+                if (versions.includes(version) && !modloader || versions.includes(modloader)) {
+                    const dl = await got.get(file.downloadUrl);
+                    const zip = new AdmZip(dl.rawBody);
+                    const lang = zip.getEntries().map(i => {
+                        if (/lang\/.+\.json/g.test(i.entryName)) {
+                            fs.mkdirSync(path.dirname(path.join(__dirname, i.entryName)), {recursive: true});
+                            if (i.entryName.endsWith("zh_cn.json") && fs.existsSync(path.join(__dirname, i.entryName)))
+                                return;
+                            fs.writeFileSync(path.join(__dirname, i.entryName), i.getData());
+                        }
+                    })
+                    break;
+                }
+
+            }
+            num++;
+        }
+        console.log(num, ret.length)
+        if (num === 0) break;
+    }
+    return ret;
+}
+
+process.nextTick(async () => {
+    console.log(await fetch_new_project(0, "1.16.1"))
+})
